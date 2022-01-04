f286b6e90d7bf5999fd7b6f44401a15561678053
jonyprill <jonathantprill@gmail.com>
2022-01-04 16:04:37 -0600

    able to fight multiple robots

--- assets/js/game.js
+++ assets/js/game.js
@@ -8,8 +8,8 @@ var enemyHealth = 50;
 var enemyAttack = 12;
 
 var fight = function (enemyName) {
-    //Alert Players that they are staring the round
-    window.alert("Welcome to Robot Gladiators!");
+    // Repeat and execute as long as the enemy robot os alive
+    while(enemyHealth > 0) {
 
     var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT or 'SKIP' to choose.");
 
@@ -52,7 +52,7 @@ var fight = function (enemyName) {
         else {
             fight();
         }
-    }
+    } }
 };
 
 
@@ -64,5 +64,7 @@ var fight = function (enemyName) {
 
 
 for(var i = 0; i < enemyNames.length; i++) {
-    fight(enemyNames[i]);
+    var pickedEnemyName = enemyNames[i];
+    enemyHealth = 50;
+    fight(pickedEnemyName);
 }
